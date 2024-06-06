ce2c3d3bf408fea44c28a80a94401dc1b70854a8
juan david iron-mind <davidtovarmontoya@gmail.com>
2024-06-06 16:47:42 -0500

    create or edit view

--- app/src/main/java/com/example/project/view/fragment/FragmentForm.kt
+++ app/src/main/java/com/example/project/view/fragment/FragmentForm.kt
@@ -1,6 +1,9 @@
 package com.example.project.view.fragment
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.os.Bundle
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Spinner
@@ -13,22 +16,23 @@ import com.example.project.databinding.FragmentFormBinding
 import com.example.project.viewmodel.PetViewModel
 
 import  androidx.appcompat.app.AppCompatActivity
-
+import java.util.Calendar
+import java.util.Date
 class FragmentForm : Fragment() {
-
+    private val calendar = Calendar.getInstance()
     private lateinit var binding: FragmentFormBinding
-    private val petViewModel: PetViewModel by viewModels()
+    private var selectedDate: String? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentFormBinding.inflate(inflater)
-        binding.spinnerHour.setSelection(4)
+        binding.datePicker.hint = "Date"
 
-        binding.spinnerHour.setSelection(1)
 
         return binding.root
     }
+
 
     override fun onResume() {
         super.onResume()
@@ -37,14 +41,40 @@ class FragmentForm : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        drivers()
+        binding.spPrioridad.setSelection(1)
+        setup()
         goBack()
     }
 
-    private fun drivers() {
+
+    private fun setup() {
 
 
+        binding.etFecha.setOnTouchListener { _, event ->
+            if (event.action == MotionEvent.ACTION_DOWN) {
+                showDatePickerDialog()
+                return@setOnTouchListener true // Consume the touch event
+            }
+            false
+        }
+    }
+    private fun showDatePickerDialog() {
+        val year = calendar.get(Calendar.YEAR)
+        val month = calendar.get(Calendar.MONTH)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
 
+        val datePickerDialog = DatePickerDialog(
+            requireContext(),
+            { _, year, monthOfYear, dayOfMonth ->
+                selectedDate = "$dayOfMonth/${monthOfYear + 1}/$year"
+                binding.etFecha.setText(selectedDate)
+            },
+            year,
+            month,
+            day
+        )
+
+        datePickerDialog.show()
     }
 
 
